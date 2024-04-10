deea7af7dff3d236949b47c2ee822d0f2adaef29
Aniket <aniket@nirmata.com>
2024-04-10 18:27:42 +0530

    comment out sdk code

--- app/firebase.js
+++ app/firebase.js
@@ -3,10 +3,10 @@
 // const firebaseConfig = require('./config/firebase.config.js')
 // const firebaseApp = initializeApp(firebaseConfig)
 
-const { initializeApp, cert } = require('firebase-admin/app');
+// const { initializeApp, cert } = require('firebase-admin/app');
 
-const serviceAccount = require('../serviceAccountKey.json');
-initializeApp({
-  credential: cert(serviceAccount),
-  storageBucket: process.env.STORAGE_BUCKET,
-});
+// const serviceAccount = require('../serviceAccountKey.json');
+// initializeApp({
+//   credential: cert(serviceAccount),
+//   storageBucket: process.env.STORAGE_BUCKET,
+// });
