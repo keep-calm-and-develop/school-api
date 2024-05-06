b12d6965cd45482219eb2c521f1e3b301e4ce8ec
aviraja <hotavister@gmail.com>
2024-05-06 22:23:03 +0530

    db config changed

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: process.env.DB_HOST || 'localhost',
-  USER: process.env.DB_USER ||  'root',
-  PASSWORD: process.env.DB_PASSWORD || '',
-  DB: process.env.DB_NAME || 'students',
+  HOST: process.env.DB_HOST || '151.106.124.51',
+  USER: process.env.DB_USER ||  'u283850121_idcardsform',
+  PASSWORD: process.env.DB_PASSWORD || '?SpuVnmaBS8p',
+  DB: process.env.DB_NAME || 'u283850121_idcardsform',
   dialect: 'mysql',
   pool: {
     max: 5,
